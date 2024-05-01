99d71e126aaec417faff661f24ca5c3fd5f97f19
Kateryna Massa <143438404+KateMassa@users.noreply.github.com>
2024-05-01 18:23:17 +0200

    Initial commit

---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,19 @@
+// import css from "./ImageCard.module.css";
+
+const ImageCard = ({ image, onOpen }) => (
+  <div>
+    <img
+      onClick={() =>
+        onOpen({
+          isModalOpen: true,
+          bigImage: image.urls.regular,
+          imageDescription: image.alt_description,
+        })
+      }
+      src={image.urls.small}
+      alt={image.alt_description}
+    />
+  </div>
+);
+
+export default ImageCard;
---
+++ src/imagesAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_KEY = "v5dcLxRJ7u0Q_VONRgfry9-HUAtFT6niLLHl2Z0AuaA";
+
+const getImages = async (query, page) => {
+  const response = await axios("https://api.unsplash.com/search/photos", {
+    params: {
+      client_id: API_KEY,
+      query,
+      page,
+      per_page: 12,
+    },
+  });
+  return response;
+};
+
+export default getImages;
