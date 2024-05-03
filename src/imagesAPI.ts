f44aea8e39e4a6d4b46571b3fb3442d9e5689c05
KateMassa <catherineorlova27@gmail.com>
2024-05-03 11:29:25 +0200

    Update imagesAPI.ts

--- src/imagesAPI.ts
+++ src/imagesAPI.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const API_KEY = "v5dcLxRJ7u0Q_VONRgfry9-HUAtFT6niLLHl2Z0AuaA";
 
-const getImages = async (query, page) => {
+const getImages: (query: string, page: number) => Promise<any> = async (
+  query,
+  page
+) => {
   const response = await axios("https://api.unsplash.com/search/photos", {
     params: {
       client_id: API_KEY,
